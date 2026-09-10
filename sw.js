8cdc9d7056865836e3b8608f5a76f689b8e7987c
ultrasonic-rail <52490637+ultrasonic-rail@users.noreply.github.com>
2026-09-10 13:27:22 +0200

    Simplify service worker install and activate logic
    Refactor service worker to simplify install and activate events.

--- sw.js
+++ sw.js
@@ -1,39 +1,5 @@
-const CACHE_NAME = 'expense-tracker-v4';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  event.waitUntil(caches.keys().then(k => Promise.all(k.map(c => caches.delete(c)))));
   self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request).then(response => {
-        // Cache new requests dynamically
-        if (response.status === 200) {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
-        }
-        return response;
-      });
-    }).catch(() => caches.match('./index.html'))
-  );
 });
